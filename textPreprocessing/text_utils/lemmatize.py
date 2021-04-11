3b64fa6198d346b10ddbbb02fd6215bab0fd0c36
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-11 20:48:56 +0300

    fix(textPreprocessing): try/except для утилиты lemmatize_word

--- textPreprocessing/text_utils/lemmatize.py
+++ textPreprocessing/text_utils/lemmatize.py
@@ -5,6 +5,9 @@ morph = pymorphy2.MorphAnalyzer()
 
 def lemmatize_word(word: str) -> str:
     """Приводит русскоязычное слово к начальной форме"""
-    p = morph.parse(word)[0] # содержит кучу различных вариантов инфинитивов
+    try:
+        p = morph.parse(word)[0] # содержит кучу различных вариантов инфинитивов
 
-    return p.normal_form  # забираем инфинитив
+        return p.normal_form  # забираем инфинитив
+    except:
+        return word
