5e7d12f24480a494d9ef728377a25cb84b27e226
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-18 13:56:12 +0300

    feat(models): в блокнотах применена утилита load_cleaned_russian_text_data + убран лишний код

--- dev/constants/repo_paths.py
+++ dev/constants/repo_paths.py
@@ -1,3 +1,3 @@
 DIRECTORY_WITH_DATA = 'data'
-ORIGINAL_RUSSIAN_DF_NAME = 'toxicCommentsOriginalDF.csv'
+ORIGINAL_RUSSIAN_DF_NAME = 'toxicRUCommentsOriginalDF.csv'
 CLEANED_RUSSIAN_DF_NAME = 'toxicRUCommentsCleanedDF.csv'
