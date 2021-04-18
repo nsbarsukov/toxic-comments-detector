e30a5db59b425b0044e4dd0f2ce4e58bc10e9d7e
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-18 13:17:38 +0300

    feat(dev): очищенный датасет с русскоязычными комментариями

--- dev/constants/repo_paths.py
+++ dev/constants/repo_paths.py
@@ -1,3 +1,3 @@
 DIRECTORY_WITH_DATA = 'data'
 ORIGINAL_RUSSIAN_DF_NAME = 'toxicCommentsOriginalDF.csv'
-CLEANED_RUSSIAN_DF_NAME = 'toxicCommentsCleanedDF.csv'
+CLEANED_RUSSIAN_DF_NAME = 'toxicRUCommentsCleanedDF.csv'
---
+++ dev/utils/clean_df_ru_texts.py
@@ -0,0 +1,28 @@
+import multiprocessing
+import pandas as pd
+from tqdm import tqdm
+
+from textPreprocessing import preprocess_text
+from dev import DIRECTORY_WITH_DATA, ORIGINAL_RUSSIAN_DF_NAME, CLEANED_RUSSIAN_DF_NAME
+
+if __name__ == "__main__":
+    """
+    Запускает очистку текста комментариев внутри датасета {DIRECTORY_WITH_DATA}/{ORIGINAL_RUSSIAN_DF_NAME},
+    применяя к каждой комментарию функцию from textPreprocessing import preprocess_text
+    """
+    STREAMS_COUNT = multiprocessing.cpu_count()
+    print('Запущен скрипт в', STREAMS_COUNT, 'потоков')
+
+    orig_toxic_comments_df = pd.read_csv(f"../../{DIRECTORY_WITH_DATA}/{ORIGINAL_RUSSIAN_DF_NAME}")
+
+    pool = multiprocessing.Pool(STREAMS_COUNT)
+
+    orig_toxic_comments_df['norm_comment'] = tqdm(
+        pool.imap(preprocess_text, orig_toxic_comments_df['comment']),
+        total=len(orig_toxic_comments_df)
+    )
+
+    orig_toxic_comments_df.to_csv(f"../../{DIRECTORY_WITH_DATA}/{CLEANED_RUSSIAN_DF_NAME}", index=False)
+
+    pool.close()
+    pool.join()
