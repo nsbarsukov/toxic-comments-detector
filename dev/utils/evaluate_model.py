041c0b87edcb2161b8f1d59dd43109ba7ae09243
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-19 21:51:09 +0300

    feat(dev): more fancy confusion matrix in evaluate_model

--- dev/utils/evaluate_model.py
+++ dev/utils/evaluate_model.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def format_to_percent(decimal_number):
@@ -14,14 +15,20 @@ def plot_history_metric_graph(history, metric):
     plt.legend([metric, 'val_' + metric])
 
 
+def plot_confusion_matrix(y_true, y_pred):
+    cm = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(5,5))
+    sns.heatmap(cm, annot=True, fmt="d")
+    plt.title('Confusion Matrix', fontdict={'fontsize': 18, 'fontweight': 500})
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+
+
 def evaluate_model(y_true, y_pred, tf_history_learning=None):
     print("Accuracy:", format_to_percent(accuracy_score(y_true, y_pred)))
     print("Recall:", format_to_percent(recall_score(y_true, y_pred)))
     print("Precision:", format_to_percent(precision_score(y_true, y_pred)))
     print("F1-score:", format_to_percent(f1_score(y_true, y_pred)), '\n')
-
-    print('Confusion matrix:')
-    print(confusion_matrix(y_true=y_true, y_pred=y_pred))
 
     if tf_history_learning is not None:
         plt.figure(figsize=(20, 10), tight_layout={'h_pad': 5})
@@ -34,3 +41,5 @@ def evaluate_model(y_true, y_pred, tf_history_learning=None):
         plot_history_metric_graph(tf_history_learning, 'recall')
         plt.subplot(2, 2, 4)
         plot_history_metric_graph(tf_history_learning, 'precision')
+
+    plot_confusion_matrix(y_true, y_pred)
