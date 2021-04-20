e2a498f207b7cf7b4c82362c718eba4139d66f49
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-20 22:42:25 +0300

    feat(wordEmbeddingsLayers): NavecVectorizerLayer update

--- wordEmbeddingsLayers/__init__.py
+++ wordEmbeddingsLayers/__init__.py
@@ -1,6 +1,6 @@
 from .custom_vectorizer_layer import CustomVectorizerLayer
 
 from .wiki_ml_model import wiki40_russian_embedding_layer
-from .navec.navec_vectorizer_layer import navec_vectorizer_layer
+from .navec.navec_vectorizer_layer import NavecVectorizerLayer
 
 
--- wordEmbeddingsLayers/navec/navec_vectorizer_layer.py
+++ wordEmbeddingsLayers/navec/navec_vectorizer_layer.py
@@ -1,14 +1,21 @@
+import tensorflow as tf
+from tensorflow.keras import layers
 from navec import Navec
+from typing import List
 
 from dev import PAD_WORD
 from wordEmbeddingsLayers import CustomVectorizerLayer
 
-navec_embeddings = Navec.load('navecWeights.tar')
 
+class NavecVectorizerLayer(layers.Layer):
+    def __init__(self, pad_word=PAD_WORD, pad_sentence_to_n_words=30):
+        super(NavecVectorizerLayer, self).__init__()
+        self.navec_embeddings = Navec.load('wordEmbeddingsLayers/navec/navecWeights.tar')
+        self.vectorizer = CustomVectorizerLayer(
+            vectorizer=lambda word: self.navec_embeddings[word],
+            pad_sentence_to_n_words=pad_sentence_to_n_words,
+            pad_word=pad_word
+        )
 
-def navec_vectorizer(word):
-    return navec_embeddings[word]
-
-
-navec_vectorizer_layer = CustomVectorizerLayer(navec_vectorizer, pad_sentence_to_n_words=3, pad_word=PAD_WORD)
-
+    def call(self, texts_arr: List[str]) -> tf.Tensor:
+        return self.vectorizer(texts_arr)
