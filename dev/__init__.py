ed7c0e87b8f2a4076bfdd60b1a7ccab1d5f2cc6d
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-24 14:27:11 +0300

    feat(dev): get_3d_quartile_words_count util

--- dev/__init__.py
+++ dev/__init__.py
@@ -6,4 +6,5 @@ from .utils.load_cleaned_russian_text_data import *
 from .utils.imbalanced_data_utils import *
 from .utils.pad_words_arr import *
 from .utils.vectorize_sentence import *
+from .utils.get_3d_quartile_words_count import *
 from .utils.evaluate_model import evaluate_model
---
+++ dev/utils/get_3d_quartile_words_count.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def get_3d_quartile_words_count(sentences_array):
+    """
+    Получает массив строк/предложений.
+    Отдает число N.
+    75% всех предложений данного массива не превышают N слов в длину.
+    """
+
+    def count_words(sentence):
+        return len(sentence.split(' '))
+
+    return round(
+        np.percentile(
+            list(map(count_words, sentences_array)),
+            75
+        )
+    )
