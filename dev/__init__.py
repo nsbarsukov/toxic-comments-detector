5ae3aa4a075b9629b2ebbd90bf6752d768182692
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-24 14:24:00 +0300

    feat(dev): vectorize_sentence + make_vectorizer утилиты

--- dev/__init__.py
+++ dev/__init__.py
@@ -5,4 +5,5 @@ from .constants.aliases import *
 from .utils.load_cleaned_russian_text_data import *
 from .utils.imbalanced_data_utils import *
 from .utils.pad_words_arr import *
+from .utils.vectorize_sentence import *
 from .utils.evaluate_model import evaluate_model
---
+++ dev/utils/vectorize_sentence.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from dev import pad_words_arr, PAD_WORD
+
+
+def vectorize_sentence(sentence: str, vectorizer, pad_word=PAD_WORD, pad_sentence_to_n_words=30) -> np.array:
+    words_arr = sentence.split(' ')
+    padded_word_arr = pad_words_arr(words_arr, maxlen=pad_sentence_to_n_words, pad_word=pad_word)
+
+    return np.array(list(map(vectorizer, padded_word_arr)))
+
+
+def make_vectorizer(vectorizer, pad_word=PAD_WORD, pad_sentence_to_n_words=30):
+    return lambda sentence: vectorize_sentence(sentence, vectorizer, pad_word, pad_sentence_to_n_words)
