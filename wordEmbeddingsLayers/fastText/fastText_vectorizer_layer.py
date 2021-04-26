10c956186438f91f4c31b1eb8b266e740f25d0e9
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-26 22:47:14 +0300

    feat(wordEmbeddingsLayers): небольшой рефакторинг с целью повышения скорости импортов

--- wordEmbeddingsLayers/fastText/fastText_vectorizer_layer.py
+++ wordEmbeddingsLayers/fastText/fastText_vectorizer_layer.py
@@ -3,17 +3,26 @@ import fasttext.util
 
 PATH_TO_FASTTEXT_WEIGHTS = 'wordEmbeddingsLayers/fastText/cc.ru.300.bin'
 
-try:
-    ft = fasttext.load_model(PATH_TO_FASTTEXT_WEIGHTS)
-except:
-    fasttext.util.download_model('ru', if_exists='ignore')
-    ft = fasttext.load_model(PATH_TO_FASTTEXT_WEIGHTS)
 
-FASTTEXT_EMBEDDING_DIMENSION = ft.get_dimension()
-
-
-def fasttext_word_vectorizer(word: str):
+def load_russian_fasttext_model():
     try:
-        return ft.get_word_vector(word)
+        ft = fasttext.load_model(PATH_TO_FASTTEXT_WEIGHTS)
     except:
-        return np.zeros(FASTTEXT_EMBEDDING_DIMENSION)
+        fasttext.util.download_model('ru', if_exists='ignore')
+        ft = fasttext.load_model(PATH_TO_FASTTEXT_WEIGHTS)
+
+    return ft
+
+
+def get_russian_fasttext_word_vectorizer():
+    ft = load_russian_fasttext_model()
+
+    FASTTEXT_EMBEDDING_DIMENSION = ft.get_dimension()
+
+    def fasttext_word_vectorizer(word: str):
+        try:
+            return ft.get_word_vector(word)
+        except:
+            return np.zeros(FASTTEXT_EMBEDDING_DIMENSION)
+
+    return fasttext_word_vectorizer, FASTTEXT_EMBEDDING_DIMENSION
--- wordEmbeddingsLayers/wiki_lm_russian/wiki_lm_russian.py
+++ wordEmbeddingsLayers/wiki_lm_russian/wiki_lm_russian.py
@@ -5,10 +5,12 @@ import tensorflow_hub as hub
 RUSSIAN = 'ru'
 RUSSIAN_WORDS_EMBEDDING_HUB_MODEL = 'https://tfhub.dev/google/wiki40b-lm-{}/1'.format(RUSSIAN)
 
-wiki40_russian_embedding_layer = hub.KerasLayer(
-    handle=RUSSIAN_WORDS_EMBEDDING_HUB_MODEL,
-    signature='word_embeddings', # модель умеет многое, но нас интересует только векторизация слов
-    output_key='word_embeddings',
-    dtype=tf.string,
-    trainable=False, # Denotes whether we want to finetune model or not. We set it to True, the embeddings present in model are finetuned based on our downstream task
-)
+
+def create_wiki40_russian_embedding_layer():
+    return hub.KerasLayer(
+        handle=RUSSIAN_WORDS_EMBEDDING_HUB_MODEL,
+        signature='word_embeddings', # модель умеет многое, но нас интересует только векторизация слов
+        output_key='word_embeddings',
+        dtype=tf.string,
+        trainable=False, # impossible to finetuned (because it is tf1 model)
+    )
