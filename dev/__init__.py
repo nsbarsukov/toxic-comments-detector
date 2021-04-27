2422b5630fd5145d00382152491b0f1809d55a05
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-04-27 21:20:14 +0300

    feat(dev): contants default_hyperparameters

--- dev/__init__.py
+++ dev/__init__.py
@@ -1,6 +1,6 @@
-from .constants.seed import SEED
 from .constants.repo_paths import *
 from .constants.aliases import *
+from .constants.default_hyperparameters import *
 
 from .utils.load_cleaned_russian_text_data import *
 from .utils.imbalanced_data_utils import *
---
+++ dev/constants/default_hyperparameters.py
@@ -0,0 +1,8 @@
+# для генератора случайных чисел
+SEED = 27
+
+# сколько от тестовой выборки отрезать под валидационную
+VALIDATION_SPLIT_PARTITION = 0.2
+
+DEFAULT_EPOCHS = 10
+DEFAULT_BATCH_SIZE = 2**9
