4622eaa13a30cce5fc0d1550f77be11fef249cb9
Barsukov Nikita <n.barsukov@tinkoff.ru>
2021-05-03 18:31:20 +0300

    feat(wordEmbeddingsLayers): BERTLayer

--- wordEmbeddingsLayers/multiBERT/multiBERT.py
+++ wordEmbeddingsLayers/multiBERT/multiBERT.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
+from tensorflow.keras import layers
 import tensorflow_text as tf_text # dont remove it: need for downloading from hub
 import tensorflow_hub as hub
+from typing import List
 
 
 multiBERT_PREPROCESS_HUB_MODEL = 'https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/3'
@@ -18,3 +20,18 @@ def create_multiBERT_encoder_layer():
         dtype=tf.string,
         trainable=True,
     )
+
+
+class BERTLayer(layers.Layer):
+    def __init__(self, to_sequence=True):
+        super(BERTLayer, self).__init__()
+        self.to_sequence = to_sequence
+        self.preprocessor = create_multiBERT_preprocess_layer()
+        self.encoder = create_multiBERT_encoder_layer()
+
+    def call(self, texts_arr: List[str]) -> tf.Tensor:
+        preprocessed_texts = self.preprocessor(texts_arr)
+        encoded_output = self.encoder(preprocessed_texts)
+
+        return encoded_output['sequence_output'] if self.to_sequence else encoded_output['pooled_output']
+
